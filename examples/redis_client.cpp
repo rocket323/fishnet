1f552ed116513a09124f85220f6260cbc7214b11
rocke323 <rocke323@gmail.com>
2019-05-22 15:27:00 +0800

    redis

--- examples/redis_client.cpp
+++ examples/redis_client.cpp
@@ -1,28 +1,57 @@
 #include "redis/redis_client.h"
+#include <cstring>
 
 using namespace std::placeholders;
+
+void OnReply2(redisReply *reply, RedisClient *client, int idx)
+{
+    if (reply == nullptr || reply->type == REDIS_REPLY_ERROR)
+    {
+        printf("redis reply error\n");
+        EventLoop::Current()->Stop();
+        return;
+    }
+    printf("%s\n", reply->str);
+
+    if (idx < 10)
+        client->Exec(std::bind(&OnReply2, _1, client, idx + 1), 1000, "PING");
+    else
+        EventLoop::Current()->Stop();
+}
 
 void OnReply(redisReply *reply, RedisClient *client, int idx)
 {
     if (reply == nullptr || reply->type == REDIS_REPLY_ERROR)
     {
         printf("redis reply error\n");
+        EventLoop::Current()->Stop();
         return;
     }
     printf("%s\n", reply->str);
-
-    if (idx < 10)
-        client->Exec(std::bind(&OnReply, _1, client, idx + 1), 1000, "PING");
-    else
-        EventLoop::Current()->Stop();
+    EventLoop::Current()->Stop();
 }
 
-int main()
+int main(int argc, char **argv)
 {
     auto loop = EventLoop::Current();
     RedisClient client(loop, InetAddr(6379));
 
-    int ret = client.Exec(std::bind(&OnReply, _1, &client, 0), 1000, "PING");
+    if (argc > 1)
+    {
+        std::vector<const char *> argv2;
+        std::vector<size_t> argvlen;
+        for (int i = 1; i < argc; i++)
+        {
+            argv2.push_back(argv[i]);
+            argvlen.push_back(strlen(argv[i]));
+        }
+
+        int ret = client.Exec(std::bind(&OnReply, _1, &client, 0), 1000, argc - 1, argv2.data(), argvlen.data());
+    }
+    else
+    {
+        int ret = client.Exec(std::bind(&OnReply2, _1, &client, 0), 1000, "PING");
+    }
 
     loop->Loop();
     return 0;
